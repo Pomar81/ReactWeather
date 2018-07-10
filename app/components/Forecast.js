0365368138bb842cc702962f0ba876bef47a12ff
Pomar <pomarsa81@gmail.com>
2018-07-10 17:48:34 +0300

    completed step 6

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,11 +1,60 @@
 import React from "react";
+import queryString from "query-string";
+import api from "../utils/api";
+
+class Forecast extends React.Component {
+    state = {
+        loading: true,
+        error: false,
+        weather: null,
+    }
+    getForecast = () => {
+        const city = queryString.parse(this.props.location.search).city;
+        this.setState({
+            loading: true,
+            weather: null,
+        });
+
+        api.getForecast(city)
+            .then((data) => {
+                this.setState({
+                    loading: false,
+                    error: false,
+                    weather: data,
+                });
+                console.log(data);
+            })
+            .catch((err) => {
+                console.dir(err)
+                this.setState({
+                    error: true,
+                });
+            })
+            .finally(() => {
+                this.setState({
+                    loading: false,
+                });
+            });
+    }
 
 
-class Forecast extends React.Component{
+    componentDidMount() {
+        this.getForecast();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.location.search !== prevProps.location.search) {
+            this.getForecast();
+        }
+    }
+
     render() {
-        return (
-            <p>Forecast page</p>
-        );
+        if (this.state.loading)
+            return (<h1 className="forecastTitle">Loading</h1>);
+        else if (this.state.error) {
+            return (<h1 className="forecastTitle">There is an error</h1>);
+        }
+        return <h1 className="complete forecastTitle">{this.state.weather.city.name}</h1>;
     }
 }
 
--- app/utils/api.js
+++ app/utils/api.js
@@ -3,24 +3,22 @@ import axios from "axios";
 const API_KEY = "3a8b2015224aac4238b0607cd353672d";
 
 function getCurrentWeather(city) {
-    axios.get(` http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&type=accurate&APPID=${API_KEY}`)
+    return axios.get(` http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&type=accurate&APPID=${API_KEY}`)
         .then((response) => {
-            console.log(response.data);
+            return response.data;
         })
         .catch((err) => {
-            console.log("error");
-            console.dir(err);
+            throw err;
         });
 }
 
 function getForecast(city) {
-    axios.get(` http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=metric&type=accurate&APPID=${API_KEY}&cnt=5`)
+    return axios.get(` http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=metric&type=accurate&APPID=${API_KEY}&cnt=5`)
         .then((response) => {
-            console.log(response.data);
+            return response.data;
         })
         .catch((err) => {
-            console.log("error");
-            console.dir(err);
+            throw err;
         });
 }
 
